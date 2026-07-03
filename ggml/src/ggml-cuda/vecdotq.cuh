458daa7b1cdb423193dfbbdd8c0adf987dc15bd3
spark <spark@sparkdeMacBook.local>
2026-07-04 01:49:33 +0800

    cuda: probe iq3 xxs mmvq vdr4 n96

--- ggml/src/ggml-cuda/vecdotq.cuh
+++ ggml/src/ggml-cuda/vecdotq.cuh
@@ -1334,7 +1334,7 @@ static __device__ __forceinline__ float vec_dot_iq2_s_q8_1(
     return d * sumi;
 }
 
-#define VDR_IQ3_XXS_Q8_1_MMVQ 2
+#define VDR_IQ3_XXS_Q8_1_MMVQ 4
 #define VDR_IQ3_XXS_Q8_1_MMQ  2
 
 static __device__ __forceinline__ float vec_dot_iq3_xxs_q8_1(
